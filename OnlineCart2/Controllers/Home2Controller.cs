e579c226bedb2f08d036adb1722bf1fd94fc6877
Dipta Messi'nheart <dip9623@outlook.com>
2019-03-21 05:06:48 +0600

    added category wise dynamic navbar.

---
+++ OnlineCart2/Controllers/Home2Controller.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using OnlineCart2;
+
+namespace OnlineCart2.Controllers
+{
+    public class Home2Controller : Controller
+    {
+        OnlineCartEntitiesDB db = new OnlineCartEntitiesDB();
+        // GET: Home2
+        public ActionResult Index()
+        {
+
+            OnlineCartEntitiesDB db =new OnlineCartEntitiesDB();
+            var p = db.tbl_product.ToList();
+
+            ViewBag.p = p;
+            var imgs = db.tbl_images.ToList();
+            ViewBag.imgs = imgs;
+
+
+            return View();
+        }
+
+        public ActionResult cat (int id) {
+            var p = db.tbl_product.Where(l => l.cid == id).ToList();
+            ViewBag.p = p;
+            var imgs = db.tbl_images.ToList();
+            ViewBag.imgs = imgs;
+
+            return View();
+        }
+
+        public ActionResult About (int id) {
+
+            return View();
+        }
+    }
+}
